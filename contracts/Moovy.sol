394a7095426ad58ae268cbf81d9e20df87f0bec3
Nikita <nikita.chernega1@gmail.com>
2023-03-06 13:48:14 +0200

    update token contract

--- contracts/Moovy.sol
+++ contracts/Moovy.sol
@@ -2,239 +2,153 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Moovy is ERC20Capped, Ownable {
-  using SafeMath for uint256;
 
+  // @dev Token generation event
   event TGEPassed();
-  event MainnetLaunched();
-  event DistributionEpochFinished(AllocationGroup group, uint256 epoch);
-  event TokenSaleEnded();
 
-  uint256 public TGETimestamp = 0;
-  uint256 public mainnetLaunchTimestamp = 0;
+  address constant private FOUNDERS = 0x3567969465d2135435e9318Ac278975472F62BAE;
+  address constant private ADVISORS = 0x00781266C0e25bfd385685a5F4c1c9C9D0D47cDc;
+  address constant private TEAM_MEMBERS = 0xA911361263eacC9579eaA29bF14056Ee887968DF;
+  address constant private KOL_REWARDS = 0xf79a429DF39E4D102d689f977C9c2d0EB4478af0;
+  address constant private PLAY_TO_EARN = 0x51fF21c11845c1C83A2e9D0C35852dA84fdA84D5;
+  address constant private ECOSYSTEM = 0x0bA88421c5F99184F060daB9158A4cEfBcF1F3c1;
+  address constant private POOL_REWARDS = 0x4236DB10650720DefaD8dC685c8Ce3266ed2247F;
+  address constant private MARKETING = 0xAA2C53096aFd874B848e420173b9f7bE4Db16107;
+  address constant private PARTNERSHIP_PROGRAM = 0x1Bc24f3b9Dd264fBC764e6d2B86B950fd0A9f709;
 
-  uint256 private _totalSupply;
-  uint8 constant private _decimals = 18;
-  uint256 constant public _maxTokens = 300000000 * 10**_decimals;
-  string private _symbol;
-  string private _name;
-  address tokenSale; // Token sale contract
+  // @dev Token decimals
+  uint8 constant private DECIMALS = 18;
+  // @dev Token generation event
+  uint256 constant private MAX_BPS = 10000;
+  // @dev Max token supply
+  uint256 constant private MAX_TOKEN_SUPPLY = 1_000_000_000 * 10**DECIMALS;
+  // @dev Token sale supply
+  uint256 constant private TOKEN_SALE_SUPPLY = 28_774_000 * 10**DECIMALS;
 
+  // @dev Token generation timestamp
+  uint256 public TGETimestamp;
+
+  // @dev Token sale contract address
+  address immutable public tokenSale;
+  // @dev Partnership program address
+
+  // @dev Group distribution data
   struct GroupData {
     uint256 cliff; // cliff period
-    uint256 unlockDelay; // delay between unlocks
-    uint256 unlockPercentage; // unlock percentage
-    mapping (address => AccountData) accounts; // whitelisted accounts
-    address[] addresses; // whitelisted addresses
+    uint256 vestingPeriod; // vesting period
+    address receiver; // receiver address
     uint256 initialUnlock; // initial unlock percentage
-    uint256 currentEpoch; // current epoch
+    uint256 balance;
+    uint256 unlockedBalance;
   }
 
+  // @dev Allocation groups
   enum AllocationGroup {
-    Team, Ecosystem, Advisor, Liquidity, Marketing, Reserve
+    Founders, Advisors, TeamMembers, KOLRewardsPool, PlayToEarn, EcosystemFund, PoolRewards, Marketing, Partnership
   }
 
-  struct AccountData {
-    uint256 balance; // total balance
-    uint256 unlockedBalance; // unlocked part of balance
-    uint256 index;
-    uint8 epoch; // account claim epoch
-  }
-
+  // @dev Allocation group data map
   mapping (AllocationGroup => GroupData) public groups;
 
-  constructor() ERC20('Moovy', 'MOV') ERC20Capped(_maxTokens * 10**18) {
+  /**
+   * @notice Constructor
+   * @param _tokenSale Token sale contract address
+   */
+  constructor(address _tokenSale) ERC20('MOIL', 'MOIL') ERC20Capped(MAX_TOKEN_SUPPLY) {
 
-    // Setup data for Team allocation
-    groups[AllocationGroup.Team].cliff = 6 * 30 days; // cliff 6 months
-    groups[AllocationGroup.Team].unlockDelay = 3 * 30 days; // time between unlocks - 3 months
-    groups[AllocationGroup.Team].initialUnlock = 0; // initial unlock 0
-    groups[AllocationGroup.Team].unlockPercentage = 1250; // percentage per unlock - 12.5%
+    tokenSale = _tokenSale;
 
-    // Setup data for Ecosystem allocation
-  groups[AllocationGroup.Ecosystem].cliff = 0; // no cliff
-    groups[AllocationGroup.Ecosystem].unlockDelay = 3 * 30 days; // time between unlocks - 3 months
-    groups[AllocationGroup.Ecosystem].initialUnlock = 0; // initial unlock 0
-    groups[AllocationGroup.Ecosystem].unlockPercentage = 1000; // percentage per unlock - 10%
+    // setup all allocations
+    groups[AllocationGroup.Founders].cliff = 2 * 30 days;
+    groups[AllocationGroup.Founders].vestingPeriod = 7 * 30 days;
+    groups[AllocationGroup.Founders].initialUnlock = 2000;
+    groups[AllocationGroup.Founders].receiver = FOUNDERS;
+    groups[AllocationGroup.Founders].balance = 100_000_000 * 10**DECIMALS;
 
-    // Setup data for Advisor allocation
-  groups[AllocationGroup.Advisor].cliff = 6 * 30 days; // cliff 6 months
-    groups[AllocationGroup.Advisor].unlockDelay = 30 days; // time between unlocks - 1 month
-    groups[AllocationGroup.Advisor].initialUnlock = 0; // initial unlock 0
-    groups[AllocationGroup.Advisor].unlockPercentage = 1000; // percentage per unlock - 10%
+    groups[AllocationGroup.Advisors].cliff = 5 * 30 days;
+    groups[AllocationGroup.Advisors].vestingPeriod = 18 * 30 days;
+    groups[AllocationGroup.Advisors].initialUnlock = 500;
+    groups[AllocationGroup.Advisors].receiver = ADVISORS;
+    groups[AllocationGroup.Advisors].balance = 20_000_000 * 10**DECIMALS;
 
-    groups[AllocationGroup.Liquidity].cliff = 0; // no cliff
-    groups[AllocationGroup.Liquidity].unlockDelay = 2 * 30 days; // time between unlocks - 2 months
-    groups[AllocationGroup.Liquidity].initialUnlock = 20; // initial unlock percentage - 20%
-    groups[AllocationGroup.Liquidity].unlockPercentage = 1000; // percentage per unlock - 10%
+    groups[AllocationGroup.TeamMembers].cliff = 4 * 30 days;
+    groups[AllocationGroup.TeamMembers].vestingPeriod = 9 * 30 days;
+    groups[AllocationGroup.TeamMembers].initialUnlock = 1000;
+    groups[AllocationGroup.TeamMembers].receiver = TEAM_MEMBERS;
+    groups[AllocationGroup.TeamMembers].balance = 30_000_000 * 10**DECIMALS;
 
-    groups[AllocationGroup.Marketing].cliff = 0; // no cliff
-    groups[AllocationGroup.Marketing].unlockDelay = 2 * 30 days; // time between unlocks - 2 months
-    groups[AllocationGroup.Marketing].initialUnlock = 10; // initial unlock percentage - 10%
-    groups[AllocationGroup.Marketing].unlockPercentage = 375; // percentage per unlock - 3.75%
+    groups[AllocationGroup.KOLRewardsPool].cliff = 12 * 30 days;
+    groups[AllocationGroup.KOLRewardsPool].vestingPeriod = 5 * 30 days;
+    groups[AllocationGroup.KOLRewardsPool].initialUnlock = 1000;
+    groups[AllocationGroup.KOLRewardsPool].receiver = KOL_REWARDS;
+    groups[AllocationGroup.KOLRewardsPool].balance = 30_000_000 * 10**DECIMALS;
 
-    groups[AllocationGroup.Reserve].cliff = 365 days; // cliff - 1 year
-    groups[AllocationGroup.Reserve].unlockDelay = 3 * 30 days; // time between unlocks 3 months
-    groups[AllocationGroup.Reserve].initialUnlock = 0; // initial unlock 0
-    groups[AllocationGroup.Reserve].unlockPercentage = 2500; // percentage per unlock
+    groups[AllocationGroup.PlayToEarn].cliff = 0;
+    groups[AllocationGroup.PlayToEarn].vestingPeriod = 6 * 30 days;
+    groups[AllocationGroup.PlayToEarn].initialUnlock = 2500;
+    groups[AllocationGroup.PlayToEarn].receiver = PLAY_TO_EARN;
+    groups[AllocationGroup.PlayToEarn].balance = 355_000_000 * 10**DECIMALS;
 
-  }
+    groups[AllocationGroup.EcosystemFund].cliff = 0;
+    groups[AllocationGroup.EcosystemFund].vestingPeriod = 12 * 30 days;
+    groups[AllocationGroup.EcosystemFund].initialUnlock = 2000;
+    groups[AllocationGroup.EcosystemFund].receiver = ECOSYSTEM;
+    groups[AllocationGroup.EcosystemFund].balance = 201_000_000 * 10**DECIMALS;
 
-  // Adds group participants
-  function addParticipants(AllocationGroup group, address[] memory participants, uint256[] memory balances) public onlyOwner{
-    require(TGETimestamp == 0, "Tokens were already allocated");
-    require(participants.length == balances.length, "Participants and balances should have the same length");
-    require(participants.length != 0, "There should be at least one participant");
+    groups[AllocationGroup.PoolRewards].cliff = 0;
+    groups[AllocationGroup.PoolRewards].vestingPeriod = 0;
+    groups[AllocationGroup.PoolRewards].initialUnlock = 10_000;
+    groups[AllocationGroup.PoolRewards].receiver = POOL_REWARDS;
+    groups[AllocationGroup.PoolRewards].balance = 70_000_000 * 10**DECIMALS;
 
-    for (uint256 i=0; i<participants.length; i++) {
-      _addParticipant(group, participants[i], balances[i]);
-    }
-  }
+    groups[AllocationGroup.Marketing].cliff = 9 * 30 days;
+    groups[AllocationGroup.Marketing].vestingPeriod = 27 * 30 days;
+    groups[AllocationGroup.Marketing].initialUnlock = 1500;
+    groups[AllocationGroup.Marketing].receiver = MARKETING;
+    groups[AllocationGroup.Marketing].balance = 144_726_000 * 10**DECIMALS;
 
-  //get group participants
-  function getGroup(AllocationGroup group) external view returns(address[] memory)  {
-    return groups[group].addresses;
-  }
+    groups[AllocationGroup.Partnership].cliff = 3 * 30 days;
+    groups[AllocationGroup.Partnership].vestingPeriod = 12 * 30 days;
+    groups[AllocationGroup.Partnership].initialUnlock = 500;
+    groups[AllocationGroup.Partnership].receiver = PARTNERSHIP_PROGRAM;
+    groups[AllocationGroup.Partnership].balance = 20_500_000 * 10**DECIMALS;
 
-  // Removes participant`s account data and his address from array of addresses
-  function removeParticipant(AllocationGroup group, address account) public onlyOwner {
-    require(TGETimestamp == 0, "Tokens were already allocated");
-
-    delete groups[group].addresses[groups[group].accounts[account].index];
-    delete groups[group].accounts[account];
   }
 
   function setTGEPassed() public onlyOwner {
     require(TGETimestamp == 0, "TGE is already passed");
     TGETimestamp = block.timestamp;
+
+    _mint(tokenSale, TOKEN_SALE_SUPPLY);
+    _mint(address(this), cap() - TOKEN_SALE_SUPPLY);
+
     emit TGEPassed();
   }
 
-  // Sets that mainnet was launched
-  function setMainnetLaunched() public onlyOwner{
-    require(TGETimestamp != 0, "Cannot set mainnet launched before TGE");
-    require(mainnetLaunchTimestamp == 0, "Mainnet is already launched");
+  function calculateVestingAmount(AllocationGroup _group) internal view returns (uint256) {
+    GroupData memory group = groups[_group];
 
-    mainnetLaunchTimestamp = block.timestamp;
+    uint256 initialUnlock = group.balance * group.initialUnlock / MAX_BPS;
+    uint256 timePassed = (block.timestamp - group.cliff - TGETimestamp) > group.vestingPeriod ? group.vestingPeriod : (block.timestamp - group.cliff - TGETimestamp);
+    uint256 pendingTokens = (group.balance - initialUnlock) * timePassed / group.vestingPeriod;
+    uint256 availableTokens = initialUnlock + pendingTokens - group.unlockedBalance;
 
-    emit MainnetLaunched();
-  }
-
-  // Adds new participant if not exists
-  function _addParticipant(AllocationGroup group, address account, uint256 balance) internal {
-    if (groups[group].accounts[account].balance == 0) {
-      groups[group].accounts[account].balance = balance;
-      groups[group].accounts[account].epoch = 0;
-      groups[group].accounts[account].index = groups[group].addresses.length;
-      groups[group].addresses.push(account);
-    }
-  }
-
-  function calculateVestingAmount(address account, AllocationGroup group) internal view returns (uint256) {
-    uint256 pendingTokens = 0;
-
-    if (groups[group].accounts[account].epoch == 0) {
-      // if cliff is passed, unlock initial percentage to account
-      pendingTokens = groups[group].accounts[account].balance * groups[group].initialUnlock / 100;
-    } else {
-      // if not first epoch, unlock default percentage for user
-      pendingTokens = groups[group].accounts[account].balance * groups[group].unlockPercentage / 100 / 100;
-    }
-
-    return pendingTokens;
-  }
-
-  // check if distribution for group is passed
-  function isDistributionPassed(AllocationGroup group) internal view returns(bool) {
-    // distribution is passed if sum of initial percentage with percentage per epochs is more then 100
-    return (groups[group].currentEpoch * groups[group].unlockPercentage / 100 + groups[group].initialUnlock) > 100;
-  }
-
-  // check if period for distribution is valid
-  function isAvailablePeriod(uint256 epochNumber, uint256 lockPeriod, uint256 initialTimestamp, uint256 delay) internal view returns (bool) {
-    // period is available if lockPeriod(cliff) + epoch passed amount * epoch delay is less then current timestamp
-    uint256 availableEpochTimestamp = lockPeriod + (delay * epochNumber) + initialTimestamp;
-    return block.timestamp > availableEpochTimestamp;
+    return availableTokens;
   }
 
   // distribute tokens for specific group
-  function distribute(AllocationGroup group) public {
-    // distribution is not started if cliff is not passed
-    require(block.timestamp >= (TGETimestamp + groups[group].cliff), "Distribution is not started yet");
-    require(!isDistributionPassed(group) , "Distribution is already passed");
+  function distribute(AllocationGroup group) public onlyOwner {
+    require(block.timestamp >= (TGETimestamp + groups[group].cliff), "[distribute]: Distribution is not started yet");
 
     GroupData storage groupData = groups[group];
-
-    require(isAvailablePeriod(
-        groupData.currentEpoch,
-        groupData.cliff,
-          TGETimestamp,
-    groupData.unlockDelay
-      ), "It's too early for distribution");
-
-    // iterate through whitelisted users and distribute tokens
-    for (uint i=0; i<groups[group].addresses.length; i++) {
-      // We distribute if user didn't claim his funds already
-      address userAddress = groups[group].addresses[i];
-      if (groups[group].accounts[userAddress].epoch <= groupData.currentEpoch && userAddress != address(0)) {
-        // Calculate and send funds to user
-        uint256 vestingAmount = calculateVestingAmount(userAddress, group);
-        _mint(userAddress, vestingAmount);
-
-        // Update user epoch
-        updateAccountData(userAddress, group, vestingAmount);
-      }
-    }
-    // increase group epoch after distribution
-    groupData.currentEpoch += 1;
+    uint256 vestingAmount = calculateVestingAmount(group);
+    _transfer(address(this), groupData.receiver, vestingAmount);
+    groupData.unlockedBalance += vestingAmount;
   }
-
-  // claim tokens for sender
-  function claim(AllocationGroup group) public {
-    require(canClaim(msg.sender, group), "You cannot claim");
-
-    uint256 vestingAmount = calculateVestingAmount(msg.sender, group);
-
-    _mint(msg.sender, vestingAmount);
-
-    updateAccountData(msg.sender, group, vestingAmount);
-  }
-
-  // check if account can claim
-  function canClaim(address account, AllocationGroup group) internal view returns (bool) {
-    AccountData storage accountData = groups[group].accounts[account];
-
-    // if unlocked balance is bigger or equal initial balance return false
-    if (accountData.unlockedBalance >= accountData.balance) return false;
-
-    // check if period for claim is available
-    return isAvailablePeriod(accountData.epoch, groups[group].cliff, TGETimestamp, groups[group].unlockDelay);
-  }
-
-  function updateAccountData(address account, AllocationGroup group, uint256 vestingAmount) internal {
-    AccountData storage accountData = groups[group].accounts[account];
-    // if vestingAmount is bigger then available balance, use available balance
-    uint256 amountToUpdate = ((accountData.balance >= (accountData.unlockedBalance + vestingAmount))) ? vestingAmount : accountData.balance - accountData.unlockedBalance;
-
-    accountData.unlockedBalance += amountToUpdate; // add unlocked amount to unlocked balance
-    accountData.epoch += 1; // increase user epoch
-  }
-
-  // send tokens to token sal contract
-  function startTokenSale(address _tokenSale) external onlyOwner {
-    _mint(_tokenSale, _maxTokens * 12 / 100);
-    tokenSale = _tokenSale;
-  }
-
-  function endTokenSale() external {
-    require(msg.sender == tokenSale, 'unauthorized contract');
-    emit TokenSaleEnded();
-  }
-
 
 
 }
